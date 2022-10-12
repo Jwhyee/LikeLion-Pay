a091ede48b6cff8f4492ea98b9b800c402052866
Jwhyee <kawnsdud@naver.com>
2022-10-12 11:43:11 +0900

    [ADD] - Add payments modules

--- src/main/java/com/ll/exam/app__2022_10_11/service/OrderService.java
+++ src/main/java/com/ll/exam/app__2022_10_11/service/OrderService.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -86,5 +87,17 @@ public class OrderService {
 
         order.setRefundDone();
         orderRepository.save(order);
+    }
+
+    public Optional<Order> findForPrintById(long id) {
+        return findById(id);
+    }
+
+    private Optional<Order> findById(long id) {
+        return orderRepository.findById(id);
+    }
+
+    public boolean actorCanSee(Member actor, Order order) {
+        return actor.getId().equals(order.getBuyer().getId());
     }
 }
