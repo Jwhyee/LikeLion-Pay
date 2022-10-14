bf4fffe20a215cfcc7de7aa0203c0d617a806f7c
Jwhyee <kawnsdud@naver.com>
2022-10-14 15:36:38 +0900

    feat : song list

--- src/main/java/com/ll/exam/app__2022_10_11/domain/song/SongRepository.java
+++ src/main/java/com/ll/exam/app__2022_10_11/domain/song/SongRepository.java
@@ -4,5 +4,8 @@ package com.ll.exam.app__2022_10_11.domain.song;
 import com.ll.exam.app__2022_10_11.domain.song.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SongRepository extends JpaRepository<Song, Long> {
+    List<Song> findAllByAuthorId(Long id);
 }
